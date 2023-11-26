f171c1648fd7457dc0b13a6c129b5ee1e267bb44
AKMN7 <anasnassar988@gmail.com>
2023-11-26 15:05:33 +0300

    React Learning Day #5

--- 02-steps/src/App.js
+++ 02-steps/src/App.js
@@ -1,5 +1,56 @@
-function App() {
-    return <div> Hello World!</div>;
+import { useState } from "react";
+
+const messages = ["Learn React ⚛️", "Apply for jobs 💼", "Invest your new income 🤑"];
+
+export default function App() {
+    return (
+        <div>
+            <Steps />
+            <Steps />
+        </div>
+    );
 }
 
-export default App;
+function Steps() {
+    const [isOpen, setIsOpen] = useState(true);
+    const [step, setStep] = useState(1);
+
+    function increment() {
+        if (step < 3) setStep((s) => s + 1);
+    }
+
+    function decrement() {
+        if (step > 1) setStep((s) => s - 1);
+    }
+
+    return (
+        <div>
+            <button className="close" onClick={() => setIsOpen((curr) => !curr)}>
+                &times;
+            </button>
+
+            {isOpen && (
+                <div className="steps">
+                    <div className="numbers">
+                        <div className={step >= 1 ? "active" : ""}>1</div>
+                        <div className={step >= 2 ? "active" : ""}>2</div>
+                        <div className={step === 3 ? "active" : ""}>3</div>
+                    </div>
+
+                    <p className="message">
+                        Step {step}: {messages[step - 1]}
+                    </p>
+
+                    <div className="buttons">
+                        <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={decrement}>
+                            Previous
+                        </button>
+                        <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={increment}>
+                            Next
+                        </button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
