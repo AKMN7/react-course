a4d4e5a62df892432c33e5591bc3230a0e80fdbf
AKMN7 <anasnassar988@gmail.com>
2023-12-03 15:39:13 +0300

    Reuseable Component

--- 05-usepopcorn/src/index.js
+++ 05-usepopcorn/src/index.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Test() {
+    const [movieRating, setMovieRating] = useState(0);
+    return (
+        <div>
+            <StarRating color="blue" onSet={setMovieRating} />
+            <p>This movie has {movieRating} stars!</p>
+        </div>
+    );
+}
 root.render(
     <React.StrictMode>
-        <App />
+        {/* <App /> */}
+        <StarRating max={10} />
+        <Test />
     </React.StrictMode>
 );
