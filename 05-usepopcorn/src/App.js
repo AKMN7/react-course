fcee7afb2e08ec44f616719781a1ccc6e2d3859c
AKMN7 <anasnassar988@gmail.com>
2023-12-04 19:02:42 +0300

    Add API KEY

--- 05-usepopcorn/src/App.js
+++ 05-usepopcorn/src/App.js
@@ -44,6 +44,8 @@ const tempWatchedData = [
 
 const average = (arr) => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const API_KEY = "c2a2f3ff";
+
 export default function App() {
     const [movies, setMovies] = useState(tempMovieData);
     const [watched, setWatched] = useState(tempWatchedData);
