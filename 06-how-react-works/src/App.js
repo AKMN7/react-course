b1c10f2eba0d516dffb1594024d93211c9a59471
AKMN7 <anasnassar988@gmail.com>
2023-12-04 17:06:11 +0300

    State Batching Concept

--- 06-how-react-works/src/App.js
+++ 06-how-react-works/src/App.js
@@ -52,8 +52,25 @@ function TabContent({ item }) {
     const [showDetails, setShowDetails] = useState(true);
     const [likes, setLikes] = useState(0);
 
+    console.log("Component Render");
+
     function handleInc() {
-        setLikes(likes + 1);
+        setLikes((likes) => likes + 1);
+    }
+
+    function handleTripleInc() {
+        setLikes((likes) => likes + 1);
+        setLikes((likes) => likes + 1);
+        setLikes((likes) => likes + 1);
+    }
+
+    function handleReset() {
+        setShowDetails(true);
+        setLikes(0);
+    }
+
+    function handleResetLater() {
+        setTimeout(handleReset, 2000);
     }
 
     return (
@@ -67,13 +84,13 @@ function TabContent({ item }) {
                 <div className="hearts-counter">
                     <span>{likes} ❤️</span>
                     <button onClick={handleInc}>+</button>
-                    <button>+++</button>
+                    <button onClick={handleTripleInc}>+++</button>
                 </div>
             </div>
 
             <div className="tab-undo">
-                <button>Undo</button>
-                <button>Undo in 2s</button>
+                <button onClick={handleReset}>Undo</button>
+                <button onClick={handleResetLater}>Undo in 2s</button>
             </div>
         </div>
     );
