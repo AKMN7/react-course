39f0badcd5f63bcc505100e40f376fe102e89edd
AKMN7 <anasnassar988@gmail.com>
2023-12-20 16:21:18 +0300

    POST Fucntionality Using React Router Actions

--- 12-fast-react-pizza/src/App.jsx
+++ 12-fast-react-pizza/src/App.jsx
@@ -5,7 +5,7 @@ import Error from "./ui/Error";
 import Menu, { loader as MenuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order, { loader as OrderLoader } from "./features/order/Order";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, { action as CreateOrderAction } from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
             { path: "/menu", element: <Menu />, loader: MenuLoader, errorElement: <Error /> },
             { path: "/cart", element: <Cart /> },
             { path: "/order/:id", element: <Order />, loader: OrderLoader, errorElement: <Error /> },
-            { path: "/order/new", element: <CreateOrder /> }
+            { path: "/order/new", element: <CreateOrder />, action: CreateOrderAction, errorElement: <Error /> }
         ]
     }
 ]);
