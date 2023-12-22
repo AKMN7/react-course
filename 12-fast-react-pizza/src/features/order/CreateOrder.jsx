32d9bb5fdafe1be9f6b44e2d4d3e996aa3b0b6de
AKMN7 <anasnassar988@gmail.com>
2023-12-22 14:29:55 +0300

    Fetching User Address

--- 12-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 12-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import store from "../../store";
 import { clear } from "../cart/cartSlice";
 
@@ -7,13 +7,16 @@ import { createOrder } from "../../services/apiRestaurant";
 
 import Button from "../../ui/Button";
 import EmptyCart from "../../features/cart/EmptyCart";
+import { fetchAddress } from "../user/userSlice";
 
 const isValidPhone = (str) => /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str);
 
 function CreateOrder() {
-    const username = useSelector((store) => store.user.username);
+    const dispatch = useDispatch();
+    const { username, status, position, address } = useSelector((store) => store.user);
     const navigation = useNavigation();
     const isSubmitting = navigation.state === "submitting";
+    const isLoadingAddress = status === "loading";
 
     const formErrors = useActionData();
 
@@ -40,8 +43,21 @@ function CreateOrder() {
                 <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
                     <label className="sm:basis-40">Address</label>
                     <div className="grow">
-                        <input className="input w-full" type="text" name="address" required />
+                        <input className="input w-full" type="text" name="address" defaultValue={address} disabled={isLoadingAddress} required />
+                        {status === "error" && <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">Unable To Fetch Address</p>}
                     </div>
+                    {!position.latitude && !position.longitude && (
+                        <Button
+                            type="small"
+                            disabled={isLoadingAddress}
+                            onClick={(e) => {
+                                e.preventDefault();
+                                dispatch(fetchAddress());
+                            }}
+                        >
+                            Current Address
+                        </Button>
+                    )}
                 </div>
 
                 <div className="mb-12 flex items-center gap-5">
@@ -53,7 +69,7 @@ function CreateOrder() {
 
                 <div>
                     <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-                    <Button disabled={isSubmitting} type="primary">
+                    <Button disabled={isSubmitting || isLoadingAddress} type="primary">
                         {isSubmitting ? "Placing order...." : "Order now"}
                     </Button>
                 </div>
