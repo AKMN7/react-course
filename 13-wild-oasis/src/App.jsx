69040796ba46640e2bec7fa6e18cbeb2f350bddd
AKMN7 <anasnassar988@gmail.com>
2023-12-24 18:40:18 +0300

    Styled Components Introduction

--- 13-wild-oasis/src/App.jsx
+++ 13-wild-oasis/src/App.jsx
@@ -1,5 +1,28 @@
+// import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Input from "./ui/Input";
+import Button from "./ui/Button";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
 function App() {
-    return <div>Hello World!</div>;
+    return (
+        <>
+            <GlobalStyles />
+            <Row>
+                <Heading as="h1">Hello World!</Heading>
+                <Button>Test</Button>
+                <Button variation="secondary" size="small">
+                    Test
+                </Button>
+            </Row>
+            <Row type="vertical">
+                <Heading as="h2">Hello World 2!</Heading>
+                <Input type="text" placeholder="Name..." />
+                <Button>Test</Button>
+            </Row>
+        </>
+    );
 }
 
 export default App;
