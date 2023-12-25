e360c89eab965741137c3b47ca0f1ae081b02b31
AKMN7 <anasnassar988@gmail.com>
2023-12-25 18:44:54 +0300

    React Query + Devtools Installation

--- 13-wild-oasis/src/App.jsx
+++ 13-wild-oasis/src/App.jsx
@@ -1,4 +1,6 @@
 import { RouterProvider, createBrowserRouter, redirect } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
@@ -55,12 +57,21 @@ const router = createBrowserRouter([
     }
 ]);
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 60 * 1000
+        }
+    }
+});
+
 function App() {
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen={false} />
             <GlobalStyles />
             <RouterProvider router={router} />
-        </>
+        </QueryClientProvider>
     );
 }
 
