efda2073deb2a50005b2bcf1c0c4b3e21c15a12b
AKMN7 <anasnassar988@gmail.com>
2023-12-28 20:13:21 +0300

    Reusable Table Init

--- 13-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 13-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -7,18 +7,7 @@ import styled from "styled-components";
 import { useCreateCabin } from "./useCreateCabin";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
-
-const TableRow = styled.div`
-    display: grid;
-    grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-    column-gap: 2.4rem;
-    align-items: center;
-    padding: 1.4rem 2.4rem;
-
-    &:not(:last-child) {
-        border-bottom: 1px solid var(--color-grey-100);
-    }
-`;
+import Table from "../../ui/Table";
 
 const Img = styled.img`
     display: block;
@@ -58,7 +47,7 @@ function CabinRow({ cabin }) {
     }
 
     return (
-        <TableRow role="row">
+        <Table.Row role="row">
             <Img src={image} alt={name} />
             <Cabin>{name} </Cabin>
             <div>Fits up to {max_capacity}</div>
@@ -80,7 +69,7 @@ function CabinRow({ cabin }) {
                     </Modal.Content>
                 </Modal>
             </div>
-        </TableRow>
+        </Table.Row>
     );
 }
 
