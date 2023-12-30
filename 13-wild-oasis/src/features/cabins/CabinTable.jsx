b080752210a51db3d5d269adec19362d7cda632c
AKMN7 <anasnassar988@gmail.com>
2023-12-30 20:37:01 +0300

    Client Side Filtering

--- 13-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 13-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -3,10 +3,21 @@ import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import { useSearchParams } from "react-router-dom";
 
 function CabinTable() {
     const { cabins, isLoading } = useCabins();
+    const [searchParams] = useSearchParams();
+
     if (isLoading) return <Spinner />;
+
+    const filter = searchParams.get("discount") || "all";
+
+    let filteredCabins;
+    if (filter === "all") filteredCabins = cabins;
+    if (filter === "with-discount") filteredCabins = cabins.filter((el) => el.discount > 0);
+    if (filter === "no-discount") filteredCabins = cabins.filter((el) => el.discount === 0);
+
     return (
         <Menus>
             <Table columns="1.2fr 1.8fr 1.4fr 1.2fr 1.2fr 0.6fr">
@@ -19,7 +30,7 @@ function CabinTable() {
                     <div></div>
                 </Table.Header>
 
-                <Table.Body data={cabins} render={(el) => <CabinRow cabin={el} key={el.id} />} />
+                <Table.Body data={filteredCabins} render={(el) => <CabinRow cabin={el} key={el.id} />} />
             </Table>
         </Menus>
     );
--- 13-wild-oasis/src/pages/Cabins.jsx
+++ 13-wild-oasis/src/pages/Cabins.jsx
@@ -2,13 +2,14 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
 import AddCabin from "../features/cabins/AddCabin";
+import CabintTableOperations from "../features/cabins/CabintTableOperations";
 
 function Cabins() {
     return (
         <>
             <Row>
                 <Heading as="h1">All cabins</Heading>
-                <p>Filter / Sort</p>
+                <CabintTableOperations />
             </Row>
             <Row type="vertical">
                 <CabinTable />
