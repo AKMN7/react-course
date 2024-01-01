2528b625ac13e3100a30b313a8bb84cf9f635802
AKMN7 <anasnassar988@gmail.com>
2024-01-01 14:31:01 +0300

    React Query Prefetching

--- 13-wild-oasis/src/features/bookings/useBookings.js
+++ 13-wild-oasis/src/features/bookings/useBookings.js
@@ -1,8 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+    const queryClient = useQueryClient();
     const [searchParams] = useSearchParams();
 
     const filterValue = searchParams.get("status");
@@ -15,5 +16,17 @@ export function useBookings() {
     const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
     const { data: { data: bookings, count } = {}, isLoading } = useQuery({ queryKey: ["bookings", filter, sort, page], queryFn: () => getBookings({ filter, sort, page }) });
+
+    // PreFetching
+    const pageCount = Math.ceil(count / 10);
+
+    if (page < pageCount) {
+        queryClient.prefetchQuery({ queryKey: ["bookings", filter, sort, page + 1], queryFn: () => getBookings({ filter, sort, page: page + 1 }) });
+    }
+
+    if (page > 1) {
+        queryClient.prefetchQuery({ queryKey: ["bookings", filter, sort, page - 1], queryFn: () => getBookings({ filter, sort, page: page - 1 }) });
+    }
+
     return { bookings, count, isLoading };
 }
